305f71d3dcdbfa881bf3658074db88234e902861
MichalKoperski <michal@koperski.cc>
2018-06-12 22:14:42 +0200

    m

--- src/main/java/com/crud/tasks/TasksApplication.java
+++ src/main/java/com/crud/tasks/TasksApplication.java
@@ -8,7 +8,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class TasksApplication {
 
 	public static void main(String[] args) {
-        TaskDto taskDto = new TaskDto(
+       /* TaskDto taskDto = new TaskDto(
                 (long)1,
                 "Test title",
                 "I want to be a coder!");
@@ -18,6 +18,6 @@ public class TasksApplication {
         String content = taskDto.getContent();
 
         System.out.println(id+" "+title+" "+content);
-	    //SpringApplication.run(TasksApplication.class, args);
+	    */SpringApplication.run(TasksApplication.class, args);
 	}
 }
--- src/main/java/com/crud/tasks/domain/TaskDto.java
+++ src/main/java/com/crud/tasks/domain/TaskDto.java
@@ -6,6 +6,8 @@ import lombok.Getter;
 @Getter
 @AllArgsConstructor
 public class TaskDto {
+
+    public TaskDto() {}
     private Long id;
     private String title;
     private String content;
