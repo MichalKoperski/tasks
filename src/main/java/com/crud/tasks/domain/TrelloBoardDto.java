1ac591564bd02f1d804b674cab18e923feaeb059
MichalKoperski <michal@koperski.cc>
2018-07-26 21:46:12 +0200

    m

--- src/main/java/com/crud/tasks/domain/TrelloBoardDto.java
+++ src/main/java/com/crud/tasks/domain/TrelloBoardDto.java
@@ -23,14 +23,11 @@ public class TrelloBoardDto {
     @JsonProperty("lists")
     private List<TrelloListDto> lists;
 
-    //public TrelloBoardDto(){}
+    public TrelloBoardDto(){}
 
     public TrelloBoardDto(String id, String name, List<TrelloListDto> lists) {
         this.id = id;
         this.name = name;
         this.lists = lists;
-    }
-    public TrelloBoardDto(){
-        this("1","aa",new ArrayList<>());
     }
 }
