7aaeef2cce6e488aafa858c5dcc59a5bfa1a2802
MichalKoperski <michal@koperski.cc>
2018-07-26 21:48:28 +0200

    m

--- src/main/java/com/crud/tasks/domain/TrelloBoard.java
+++ src/main/java/com/crud/tasks/domain/TrelloBoard.java
@@ -5,11 +5,19 @@ import lombok.Getter;
 
 import java.util.List;
 
-@AllArgsConstructor
+//@AllArgsConstructor
 @Getter
 public class TrelloBoard {
 
     private String id;
     private String name;
     private List<TrelloList> lists;
+
+    public TrelloBoard(){}
+
+    public TrelloBoard(String id, String name, List<TrelloList> lists) {
+        this.id = id;
+        this.name = name;
+        this.lists = lists;
+    }
 }
