410dd2bcae5a52ccafb8be73a736fcee4c5030e2
MichalKoperski <michal@koperski.cc>
2018-08-14 21:41:57 +0200

    m

--- src/main/java/com/crud/tasks/TasksApplication.java
+++ src/main/java/com/crud/tasks/TasksApplication.java
@@ -1,20 +1,19 @@
 package com.crud.tasks;
 
+import com.crud.tasks.domain.TaskDto;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-<<<<<<< HEAD
-//public class TasksApplication extends SpringBootServletInitializer {
-public class TasksApplication {
-=======
 public class TasksApplication extends SpringBootServletInitializer {
->>>>>>> a05afd488e576bcb9928f6cd3d14384c2b087796
+//public class TasksApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(TasksApplication.class, args);
 	}
-	//    @Override
-	//   protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-	//        return application.sources(TasksApplication.class);
-	//}
+	    @Override
+	   protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+	        return application.sources(TasksApplication.class);
+	}
 }
